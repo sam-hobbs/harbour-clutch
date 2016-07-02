fbd0d6007f3e4a1e54e055b65d7eb180522b1c4e
Åke Engelbrektson <eson57@users.noreply.github.com>
2016-07-02 18:51:58 +0200

    Create harbour-clutch-sv.ts
    Swedish translation if wanted.

---
+++ translations/harbour-clutch-sv.ts
@@ -0,0 +1,98 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>License</source>
+        <translation>Licens</translation>
+    </message>
+    <message>
+        <source>About Clutch</source>
+        <translation>Om Clutch</translation>
+    </message>
+    <message>
+        <source>A Silica frontend for the Transmission Torrent Client</source>
+        <translation>Ett Silica grafiskt gränsnitt för Transmission torrent-klient</translation>
+    </message>
+    <message>
+        <source>Both Clutch and Transmission are open source software licensed under the terms of the GNU General Public License v2+ </source>
+        <translation>Både Clutch och Transmission är öppen källkod, licensierad under vilkoren för GNU General Public Licens v2+ </translation>
+    </message>
+    <message>
+        <source>Source (Clutch)</source>
+        <translation>Källkod (Clutch)</translation>
+    </message>
+    <message>
+        <source>Source (Transmission)</source>
+        <translation>Källkod (Transmission)</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Clutch</source>
+        <translation>Clutch</translation>
+    </message>
+</context>
+<context>
+    <name>FilePicker</name>
+    <message>
+        <source>Android storage</source>
+        <translation>Android-lagring</translation>
+    </message>
+    <message>
+        <source>SD Card</source>
+        <translation>SD-kort</translation>
+    </message>
+    <message>
+        <source>Home</source>
+        <translation>Hem</translation>
+    </message>
+    <message>
+        <source>Send %1 file(s)</source>
+        <translation>Skicka %1 fil(er)</translation>
+    </message>
+    <message>
+        <source>Back</source>
+        <translation>Tillbaka</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <source>Clutch</source>
+        <translation>Clutch</translation>
+    </message>
+    <message>
+        <source>Web UI</source>
+        <translation>Webbgränssnitt</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>Not implemented</source>
+        <translation>Inte implementerat</translation>
+    </message>
+</context>
+<context>
+    <name>WebRemote</name>
+    <message>
+        <source>Transmission Web UI</source>
+        <translation>Transmission webbgränssnitt</translation>
+    </message>
+</context>
+</TS>
