5fe1be85efadbdd86ba224064e7e8a5a3b5b7c03
CARLOS   GONZALEZ <carlosgonz@protonmail.com>
2017-01-21 19:27:30 -0500

    Create harbour-clutch-es.ts

---
+++ harbour-clutch-es.ts
@@ -0,0 +1,98 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>License</source>
+        <translation>Licensia</translation>
+    </message>
+    <message>
+        <source>About Clutch</source>
+        <translation>Acerca de Clutch</translation>
+    </message>
+    <message>
+        <source>A Silica frontend for the Transmission Torrent Client</source>
+        <translation>Una interfaz nativa para cliente de torrente transmission</translation>
+    </message>
+    <message>
+        <source>Both Clutch and Transmission are open source software licensed under the terms of the GNU General Public License v2+</source>
+        <translation>Ambos clutch y transmission son código fuente libre,bajo GNU licencia publica general v2+</translation>
+    </message>
+    <message>
+        <source>Source (Clutch)</source>
+        <translation>Fuente (Clutch)</translation>
+    </message>
+    <message>
+        <source>Source (Transmission)</source>
+        <translation>Fuente (Transmission) </translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Clutch</source>
+        <translation>Embragar</translation>
+    </message>
+</context>
+<context>
+    <name>FilePicker</name>
+    <message>
+        <source>Android storage</source>
+        <translation>Almacenamiento android</translation>
+    </message>
+    <message>
+        <source>SD Card</source>
+        <translation>Sd card</translation>
+    </message>
+    <message>
+        <source>Home</source>
+        <translation>Inicio</translation>
+    </message>
+    <message>
+        <source>Send %1 file(s)</source>
+        <translation>Enviar %1 fichero(s)</translation>
+    </message>
+    <message>
+        <source>Back</source>
+        <translation>Regresar</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Configuraciones</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Acerca de</translation>
+    </message>
+    <message>
+        <source>Clutch</source>
+        <translation>Embragar</translation>
+    </message>
+    <message>
+        <source>Web UI</source>
+        <translation>Interfaz web</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Configuraciones</translation>
+    </message>
+    <message>
+        <source>Not implemented</source>
+        <translation>No implementado</translation>
+    </message>
+</context>
+<context>
+    <name>WebRemote</name>
+    <message>
+        <source>Transmission Web UI</source>
+        <translation>Transmision por interfaz web</translation>
+    </message>
+</context>
+</TS>
